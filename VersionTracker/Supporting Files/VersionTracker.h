e6c67eee8e76bd9dc19c505c21465a8892731c99
Tom Baranes <tom.baranes@gmail.com>
2017-07-21 10:44:36 +0200

    Making one unified target for every platforms

--- VersionTracker/Supporting Files/VersionTracker.h
+++ VersionTracker/Supporting Files/VersionTracker.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Tom Baranes. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for VersionTracker.
 FOUNDATION_EXPORT double VersionTrackerVersionNumber;
